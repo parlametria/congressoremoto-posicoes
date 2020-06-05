1d7436d6437b25555a1e7cd03b75d86446d19dc2
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2020-06-05 11:43:07 -0300

    Busca dados, descritivo de votações

---
+++ README.md
@@ -0,0 +1,17 @@
+# Governismo na Câmara e Senado
+
+
+## Para baixar os dados brutos da Câmara
+
+```
+cd data/raw
+sh ./fetch.sh
+```
+
+## Criar datasets de votações
+
+
+## Congresso Remoto
+
+Esse repositório foi criado como parte do projeto Congresso Remoto, parceria entre o Parlametria, a Pulso Público e o Observatório do Legislativo Brasileiro, financiado pela Laudes Foundation.
+
---
+++ data/raw/fetch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+
+targets="votacoesVotos-2019.csv votacoes-2020.csv"
+for f in $targets; do
+	URL=https://dadosabertos.camara.leg.br/arquivos/votacoesVotos/csv/$f
+	echo "Baixando $URL"
+	curl $URL --output $f 
+done
+
+targets="votacoesOrientacoes-2020.csv votacoesOrientacoes-2019.csv"
+for f in $targets; do
+	URL=https://dadosabertos.camara.leg.br/arquivos/votacoesOrientacoes/csv/$f
+	echo "Baixando $URL"
+	curl $URL --output $f 
+done
+
+targets="votacoesVotos-2020.csv votacoesVotos-2019.csv"
+for f in $targets; do
+	URL=https://dadosabertos.camara.leg.br/arquivos/votacoesVotos/$f
+	echo "Baixando $URL"
+	curl $URL --output $f 
+done
